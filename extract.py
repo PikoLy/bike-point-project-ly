d5efced2bd9fccc34923035a7f33633590a07389
Davy <davyly@gmx.de>
2026-07-08 16:18:36 +0100

    Adding logs

--- extract.py
+++ extract.py
@@ -5,15 +5,29 @@ from datetime import datetime
 import time
 import logging
 
-# set up our variables
+#set up logging
+timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+log_dir ='logs'
+os.makedirs(log_dir, exist_ok =True)
+log_filename = f'{log_dir}/{timestamp}'
 
+logging.basicConfig(
+    filename=log_filename,
+    format= '%(asctime)s - %(levelname)s - %(message)s',
+    level = logging.INFO
+)
+
+logger = logging.getLogger()
+logger.info('Logger successfully initialized')
+
+# set up our variables
 url = 'https://api.tfl.gov.uk/BikePoint/'
 #creating a data directory and error handling, naming the folder data
 data_dir = 'data'
 #creating the directory/folder
 os.makedirs('data', exist_ok=True)
 # creating timestamp for filename and format datetime as a string
-timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+
 filename = f'{data_dir}/{timestamp}.json'
 max_retry = 5
 attempt = 0
@@ -31,16 +45,20 @@ while attempt < max_retry:
         with open(filename,'w') as file:
             json.dump(data,file)
         print('yay🥳')
+        logger.info(f'File {filename} was successfully saved')
         #stop the loop
         break
     elif status <=100 or status >=500:
         #retry but after delay
         time.sleep(delay)
         print('retrying')
+        logger.info(f'Status code {status}. Retrying. This was attempt {attempt}')
         attempt+=1
+       
     else:
         print('fix something')
         print(status)
+        logger.error(f'Error. Status code {status}. Fix it')
         break
 
 
